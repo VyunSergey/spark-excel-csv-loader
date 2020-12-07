6c2a72930972bff1bd61f24b0f886d9455e12183
Vyun Sergey <vyun.sergey@icloud.com>
2020-12-07 11:39:51 +0300

    add convertPath

--- src/main/scala/com/vyunsergey/sparkexcelcsvloader/config/Configuration.scala
+++ src/main/scala/com/vyunsergey/sparkexcelcsvloader/config/Configuration.scala
@@ -2,11 +2,17 @@ package com.vyunsergey.sparkexcelcsvloader.config
 
 import pureconfig.{ConfigReader, ConfigSource}
 import pureconfig.generic.semiauto.deriveReader
+import java.io.File
+import java.nio.file.{Path, Paths}
 
 object Configuration {
   lazy val config: AppConf = ConfigSource.default.loadOrThrow[AppConf]
 
   case class AppConf(sparkConf: Map[String, String])
+
+  def convertPath(path: String): Path = {
+    Paths.get(new File(path).toURI)
+  }
 
   implicit val appConfReader: ConfigReader[AppConf] = deriveReader[AppConf]
 }
