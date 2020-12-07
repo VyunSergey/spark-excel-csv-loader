373050cf7a290339a472dab721bc3327f2101499
Vyun Sergey <vyun.sergey@icloud.com>
2020-12-07 12:45:06 +0300

    lazy data frames

--- src/test/scala/com/vyunsergey/sparkexcelcsvloader/data/TestDataFrame.scala
+++ src/test/scala/com/vyunsergey/sparkexcelcsvloader/data/TestDataFrame.scala
@@ -17,24 +17,23 @@ case class TestDataFrame(spark: SparkSession) {
   }
 
   val simpleDfPath: Path = Configuration.convertPath("src/test/resources/read/csv/test1.csv")
-  val simpleDf: DataFrame =
-    Reader.csv(simpleDfPath)(spark)
+  lazy val simpleDf: DataFrame = Reader.csv(simpleDfPath)(spark)
 
-  val integersDf: DataFrame =
+  lazy val integersDf: DataFrame =
     spark.createDataFrame(TestRow.genIntegersSeqRows(100))
 
-  val numbersDf: DataFrame =
+  lazy val numbersDf: DataFrame =
     spark.createDataFrame(TestRow.genNumbersSeqRows(100))
 
-  val decimalDf: DataFrame =
+  lazy val decimalDf: DataFrame =
     spark.createDataFrame(TestRow.genDecimalSeqRows(100))
 
-  val stringsDf: DataFrame =
+  lazy val stringsDf: DataFrame =
     spark.createDataFrame(TestRow.genStringsSeqRows(100, 100))
 
-  val dateTimeDf: DataFrame =
+  lazy val dateTimeDf: DataFrame =
     spark.createDataFrame(TestRow.genDateTimeSeqRow(100))
 
-  val randomDf: DataFrame =
+  lazy val randomDf: DataFrame =
     spark.createDataFrame(TestRow.genRandomSeqRows(100, 100))
 }
