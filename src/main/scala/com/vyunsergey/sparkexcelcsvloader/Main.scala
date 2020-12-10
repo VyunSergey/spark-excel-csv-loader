bdf86f1356ed46c78210b264fb4a913a4277104d
Vyun Sergey <vyun.sergey@icloud.com>
2020-12-10 17:36:50 +0300

    add configs and logger

--- src/main/scala/com/vyunsergey/sparkexcelcsvloader/Main.scala
+++ src/main/scala/com/vyunsergey/sparkexcelcsvloader/Main.scala
@@ -1,13 +1,21 @@
 package com.vyunsergey.sparkexcelcsvloader
 
 import com.vyunsergey.sparkexcelcsvloader.config.Configuration
+import com.vyunsergey.sparkexcelcsvloader.reader.ReaderConfig
 import com.vyunsergey.sparkexcelcsvloader.spark.{SparkConfig, SparkConnection}
+import com.vyunsergey.sparkexcelcsvloader.writer.WriterConfig
+import org.apache.log4j.{LogManager, Logger}
+import org.apache.spark.sql.SparkSession
 
 object Main extends App {
   lazy val conf = Configuration.config
   lazy val sparkConf = SparkConfig.make(conf)
-  lazy val spark = SparkConnection.make("App")(sparkConf)
+  lazy val readerConf: ReaderConfig = ReaderConfig.make(conf)
+  lazy val writerConf: WriterConfig = WriterConfig.make(conf)
+  implicit lazy val spark: SparkSession = SparkConnection.make("App")(sparkConf)
+  implicit lazy val logger: Logger = LogManager.getLogger(getClass)
 
-  println(spark.version)
+  logger.info(s"Spark Version: ${spark.version}")
+
   spark.stop()
 }
