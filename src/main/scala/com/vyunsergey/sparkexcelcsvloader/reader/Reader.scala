03348e5c90885fe6bb8a39391133502a178faef9
Vyun Sergey <vyun.sergey@icloud.com>
2021-03-11 18:32:17 +0300

    optimize Reader.zip method

--- src/main/scala/com/vyunsergey/sparkexcelcsvloader/reader/Reader.scala
+++ src/main/scala/com/vyunsergey/sparkexcelcsvloader/reader/Reader.scala
@@ -2,12 +2,11 @@ package com.vyunsergey.sparkexcelcsvloader.reader
 
 import org.apache.log4j.Logger
 import org.apache.spark.input.PortableDataStream
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import java.nio.file.Path
-import java.io.{BufferedReader, InputStream, InputStreamReader}
-import java.util.zip.ZipInputStream
+import java.io.{BufferedReader, InputStreamReader}
+import java.util.zip.{ZipEntry, ZipInputStream}
 
 object Reader {
   def csv(path: Path)(readerConf: ReaderConfig)
@@ -45,33 +44,28 @@ object Reader {
     logger.info(s"Read Zip archive from path: ${path.toUri.getPath}")
     logger.info(s"Reader options:\n${readerConf.csvOptions().mkString("\n")}")
 
-    val zipEntities: RDD[(Int, String)] = spark.sparkContext.binaryFiles(path.toUri.getPath)
+    val zipInputStreams: Stream[(String, PortableDataStream)] = spark.sparkContext
+      .binaryFiles(path.toUri.getPath)
+      .collect.toStream
+
+    val df: DataFrame = zipInputStreams
       .flatMap { case (_: String, content: PortableDataStream) =>
         val zis = new ZipInputStream(content.open)
-        Stream.continually(zis.getNextEntry).zipWithIndex
+        Stream.continually(zis.getNextEntry)
           .takeWhile {
-            case (null, _) => zis.close(); false
+            case null =>
+              zis.close()
+              false
             case _ => true
           }
-          .flatMap { case (_, i) =>
+          .map { _: ZipEntry =>
             val br = new BufferedReader(new InputStreamReader(zis))
-            Stream.continually(br.readLine())
-              .takeWhile(_ != null)
-              .map((i, _))
+            val lines = Stream.continually(br.readLine()).takeWhile(_ != null)
+            spark.read
+              .options(readerConf.csvOptions())
+              .csv(spark.createDataset(spark.sparkContext.parallelize(lines)))
           }
-      }
-
-    val df: DataFrame = zipEntities
-      .groupByKey
-      .collect
-      .toMap
-      .mapValues { lines =>
-        spark.read
-          .options(readerConf.csvOptions())
-          .csv(spark.createDataset(spark.sparkContext.parallelize(lines.toSeq)))
-      }
-      .values
-      .reduce(_ union _)
+      }.reduce(_ union _)
 
     logger.info(s"Read DataFrame with schema:\n${df.schema.treeString}")
     df
